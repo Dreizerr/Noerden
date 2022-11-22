64296ead2e34ad49c92eb0ac86d84fbfb8425110
Dreizer <89171351+Dreizerr@users.noreply.github.com>
2022-11-22 16:44:50 +0200

    Лайки

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 let sliderImages = document.querySelectorAll(".watch__img");
 let arrowLeft = document.querySelector(".arrow-1");
 let arrowRight = document.querySelector(".arrow-2");
+let likes = document.querySelectorAll(".review__like-img");
+let commentOpeners = document.querySelectorAll(".review__read-next");
+
+for (let commentOpener of commentOpeners) {
+  commentOpener.onclick = function () {
+    if (commentOpener.parentNode.classList.contains("active")) {
+      commentOpener.innerHTML = "Читать далее...";
+    }
+    else {
+      commentOpener.innerHTML = "Скрыть";
+    }
+    commentOpener.parentNode.classList.toggle("active");
+  }
+}
+
+for (let like of likes) {
+  like.nextElementSibling.innerHTML = Math.floor(Math.random() * 99);
+  like.onclick = function () {
+    if (like.classList.contains("active")) {
+      like.nextElementSibling.innerHTML--;
+    } else {
+      like.nextElementSibling.innerHTML++;
+    }
+    like.classList.toggle("active");
+  };
+}
 
 arrowLeft.onclick = function () {
   for (let img of sliderImages) {
